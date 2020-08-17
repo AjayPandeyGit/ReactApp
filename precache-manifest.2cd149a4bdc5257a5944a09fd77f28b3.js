3eae6bceedefdadcdc0d2a992b42fcf46c817efd
ajay pandey <ajaypandey348@gmail.com>
2020-08-18 00:34:38 +0530

    Updates

---
+++ precache-manifest.2cd149a4bdc5257a5944a09fd77f28b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73afd9b35d5c6fcd4deebfe0e6eee927",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "f6d8554079e373128658",
+    "url": "/ReactApp/static/css/main.da538cd8.chunk.css"
+  },
+  {
+    "revision": "4396f6bb2e37a70bfb69",
+    "url": "/ReactApp/static/js/2.ccfd08fb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactApp/static/js/2.ccfd08fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d8554079e373128658",
+    "url": "/ReactApp/static/js/main.03ea8292.chunk.js"
+  },
+  {
+    "revision": "32f4b6810f280f4985f7",
+    "url": "/ReactApp/static/js/runtime-main.b6384193.js"
+  }
+]);
